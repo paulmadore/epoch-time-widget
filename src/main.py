48229c9ad1c137af5af72581f7f395abb2eb3d42
paul.madore <phm@woodcoin.org>
2017-02-20 01:12:38 -0600

    now working properly

--- src/main.py
+++ src/main.py
@@ -1,39 +1,39 @@
 import gi
-import pygtkcompat
-import time
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+class ButtonWindow(Gtk.Window):
 
-class MainWindow(Gtk.Window):
-    """defines the main window"""
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Button Demo")
+        self.set_border_width(10)
 
-    def __init__(self, *args, **kwargs):
-        pass
-        
-    def on_window_main_destroy(self, *args):
-        Gtk.main_quit(*args)
-        
-    def on_get_epoch_clicked(self, button):
-        builder = Gtk.Builder()
-        builder.add_from_file("../glade/window_main_2.glade")
-        label = builder.get_object('show_epoch')
-        current_epoch = str(int(time.time()))
-        label.set_selectable(True)
-        label.set_css_name('epoch_time_output')
-        print(current_epoch)
-        label.set_text(current_epoch)
+        hbox = Gtk.Box(spacing=6)
+        self.add(hbox)
 
-    def operate(object):
-        builder = Gtk.Builder()
-        builder.add_from_file("../glade/window_main_2.glade")
-        window = builder.get_object("window_main")
-        css_provider = Gtk.CssProvider()
-        css_provider.load_from_path('epoch.css')
-        builder.connect_signals(MainWindow())
-        window.show_all()
-        Gtk.main()
+        button = Gtk.Button.new_with_label("Click Me")
+        button.connect("clicked", self.on_click_me_clicked)
+        hbox.pack_start(button, True, True, 0)
 
-if __name__ == '__main__':
-    MainWindow().operate()
-    
+        button = Gtk.Button.new_with_mnemonic("_Open")
+        button.connect("clicked", self.on_open_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+        button = Gtk.Button.new_with_mnemonic("_Close")
+        button.connect("clicked", self.on_close_clicked)
+        hbox.pack_start(button, True, True, 0)
+
+    def on_click_me_clicked(self, button):
+        print("\"Click me\" button was clicked")
+
+    def on_open_clicked(self, button):
+        print("\"Open\" button was clicked")
+
+    def on_close_clicked(self, button):
+        print("Closing application")
+        Gtk.main_quit()
+
+win = ButtonWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
